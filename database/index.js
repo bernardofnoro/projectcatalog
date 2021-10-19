ad67b733f22b52867bd891f89fdcac88a49a3327
bernardofnoro <bernardofn@outlook.com>
2021-10-19 19:32:15 -0300

    .

--- database/index.js
+++ database/index.js
@@ -1,17 +1,31 @@
-const {Sequelize} = require("sequelize");
+// const {Sequelize} = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DB_BASE,process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+// const sequelize = new Sequelize(process.env.DB_BASE,process.env.DB_USER, process.env.DB_PASS, {
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   dialect: 'postgres',
+// });
+
+// async function conectado(){
+// try {
+//     await sequelize.authenticate();
+//     console.log('Connection has been established successfully.');
+//   } catch (error) {
+//     console.error('Unable to connect to the database:', error);
+//   }
+// }
+
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {     
   dialect: 'postgres',
-});
+  protocol: 'postgres',     
+  dialectOptions: {         
+    ssl: {             
+      require: true,             
+      rejectUnauthorized: false         
+    }     
+  } 
+}) 
 
-async function conectado(){
-try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-}
-module.exports = {sequelize,conectado};
+module.exports = sequelize;
